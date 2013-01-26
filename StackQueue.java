943e31c76934f4c1908ccf514e2fc58d86af8e28
Delos Chang <lol.i.laugh@gmail.com>
2013-01-26 16:48:57 -0500

    adding more package definitions

--- StackQueue.java
+++ StackQueue.java
@@ -1,1 +1,80 @@
 package SA6;
+
+/**
+ * StackQueue.java
+ *
+ * Implements queue using two stacks
+ * @author Delos Chang - 1/25/13
+ */
+
+public class StackQueue<T> implements CS10Queue<T> {
+	
+	public void enqueue(T item);
+	
+	/**
+	 * Remove item from front of queue
+	 * @return the item removed from the front of the queue
+	 */
+	public T dequeue();
+	
+	/**
+	 * Return the item at the front of queue, but do not remove it
+	 * @return the item at the front of the queue
+	 */
+	public T front();
+	
+	/**
+	 * Is the queue empty?
+	 * @return true iff queue is empty
+	 */
+	public boolean isEmpty();
+	
+	/**
+	 * A testing program
+	 */
+	 public static void main (String [] args)  {
+	   CS10Queue<String> q = new StackQueue<String>();
+	   System.out.println("Is it empty? : " + q.isEmpty());
+	   q.enqueue("cat");
+	   q.enqueue("dog");
+	   q.enqueue("bee");
+	   System.out.println("Is it empty? : " + q.isEmpty());
+	   System.out.println("Front is: " + q.dequeue());
+	   System.out.println("Next front is: " + q.dequeue());
+	   System.out.println("Is it empty? : " + q.isEmpty());
+	   q.enqueue("eagle");
+	   System.out.println("Next front is: " + q.dequeue());
+	   System.out.println("Next front is: " + q.dequeue());
+	   System.out.println("Is it empty? : " + q.isEmpty());
+	   System.out.println("dequeue of empty stack: " + q.dequeue());
+	   
+	   q.enqueue("bear");
+	   q.enqueue("beaver");
+	   System.out.println("front is: " + q.dequeue());
+	   q.enqueue("cat");
+	   q.enqueue("dog");
+	   q.enqueue("sheep");
+	   q.enqueue("cow");
+	   q.enqueue("eagle");
+	   q.enqueue("bee");
+	   q.enqueue("lion");
+	   q.enqueue("tiger");
+	   q.enqueue("zebra");
+	   q.enqueue("ant");
+	   
+	   System.out.println("Bigger example:");
+	   System.out.println("front is: " + q.dequeue());
+	   System.out.println("Next front is: " + q.dequeue());
+	   System.out.println("Next front is: " + q.dequeue());
+	   System.out.println("Next front is: " + q.dequeue());
+	   System.out.println("Next front is: " + q.dequeue());
+	   System.out.println("Next front is: " + q.dequeue());
+	   System.out.println("Next front is: " + q.dequeue());
+	   System.out.println("Next front is: " + q.dequeue());
+	   System.out.println("Next front is: " + q.dequeue());
+	   System.out.println("Next front is: " + q.dequeue());
+	   System.out.println("Next front is: " + q.dequeue());
+	   System.out.println("Is it empty? : " + q.isEmpty());
+	   System.out.println("Next front is: " + q.dequeue());
+	 }
+}
